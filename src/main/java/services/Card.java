6e343c0e3b8319b36b7e9232222051a505856975
Vetal777 <pro100vetal07030401@gmail.com>
2022-08-15 14:17:34 +0300

    ONE-TWO

--- src/main/java/services/Card.java
+++ src/main/java/services/Card.java
@@ -4,5 +4,7 @@ public class Card {
 
     private void message(){
 
+        System.out.println("ABC-12345");
+        System.out.println("________-------");
     }
 }
--- src/main/java/services/DeliveryServiceImpl.java
+++ src/main/java/services/DeliveryServiceImpl.java
@@ -4,5 +4,6 @@ public class DeliveryServiceImpl implements DeliverService{
     @Override
     public void delivery() {
 
+        System.out.println("Something newg");
     }
 }
