6c59c9480e56c042b7a752f7e0c8199111157e3b
Vetal777 <pro100vetal07030401@gmail.com>
2022-08-16 00:34:47 +0300

    master

--- src/main/java/services/Card.java
+++ src/main/java/services/Card.java
@@ -3,9 +3,10 @@ package services;
 public class Card {
 
     private void message(){
-
         System.out.println("ABC-12345");
         System.out.println("________-------");
         System.out.println("abc");
+        System.out.println("master");
+
     }
 }
